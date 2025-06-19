9200a27a5954b129de415a827acf5f5abb9fecb8
alejandro_gonzalez2 <alejandro_gonzalez2@epam.com>
2025-06-19 12:53:36 +0200

    Initial Commit

---
+++ GroundPass/GroundPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroundPassApp.swift
+//  GroundPass
+//
+//  Created by Alejandro Gonzalez2 on 19/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GroundPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroundPassTests/GroundPassTests.swift
@@ -0,0 +1,16 @@
+//
+//  GroundPassTests.swift
+//  GroundPassTests
+//
+//  Created by Alejandro Gonzalez2 on 19/6/25.
+//
+
+import Testing
+
+struct GroundPassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
