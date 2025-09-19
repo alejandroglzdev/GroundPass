84832345a12f0dc43e4b1e0213b38765537b0056
Alejandro González Espejo <alejandro.glz.developer@gmail.com>
2025-09-19 13:39:32 +0200

    Search callbackd done.

--- GroundPass/Views/SearchSatelliteView.swift
+++ GroundPass/Views/SearchSatelliteView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SearchSatelliteView: View {
     @State private var searchText = ""
+    @State private var isUserWritingTimer: DispatchWorkItem?
     
     var body: some View {
         let popularSatellites = PopularSatellites.all
@@ -18,6 +19,19 @@ struct SearchSatelliteView: View {
         
         VStack(alignment: .leading) {
             SearchBar(text: $searchText)
+                .onChange(of: searchText) { oldValue, newValue in
+                    isUserWritingTimer?.cancel()
+                    
+                    let task = DispatchWorkItem {
+                        // Search logic
+                        print("Searching for... \(newValue)")
+                    }
+                    
+                    isUserWritingTimer = task
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: task)
+                }
+            
             HeaderText(text: L10n.SearchSatelliteView.popularSatelliteTitle)
                 .padding(.vertical, 18)
             
