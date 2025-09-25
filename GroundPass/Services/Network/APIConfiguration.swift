639a39e777decdfecb97bb03516da7231b295fe7
Alejandro González Espejo <alejandro.glz.developer@gmail.com>
2025-09-25 16:52:04 +0200

    Some configuration.

--- GroundPass/Services/Network/APIConfiguration.swift
+++ GroundPass/Services/Network/APIConfiguration.swift
@@ -5,9 +5,11 @@
 //  Created by Alejandro Gonzalez2 on 26/8/25.
 //
 
+import Foundation
+
 enum APIConfiguration {
     static let baseURLN2YO = "https://api.n2yo.com/rest/v1/satellite"
-    static let apiKeyN2YO = "E9J7HC-MHSK9S-KL2N3Y-5JB0"
+    static let apiKeyN2YO = Bundle.main.infoDictionary?["API_KEY"] as? String
     
     static let baseURLGroundPassBackend = "http://localhost:3000"
     
